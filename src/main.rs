37065492bfed5ae55dd16ad301379f548daaca6b
Isaac Woods <isaacwoods.home@gmail.com>
2023-01-14 16:11:15 +0000

    Start very basic parsing + start evaluating expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chumsky::prelude::*;
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
@@ -9,6 +10,37 @@ fn main() -> io::Result<()> {
         // Get a line of input
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        println!("Got line: {}", input);
+        let ast = parser().parse(input);
+        println!("AST: {:?}", ast);
+
+        if let Ok(expr) = ast {
+            println!("Result: {:?}", expr.eval());
+        }
     }
+}
+
+#[derive(Clone, Debug)]
+enum Expr {
+    Num(f64),
+
+    Neg(Box<Expr>),
+    Add(Box<Expr>, Box<Expr>),
+}
+
+impl Expr {
+    pub fn eval(self) -> Result<f64, String> {
+        match self {
+            Expr::Num(value) => Ok(value),
+            Expr::Neg(a) => Ok(-a.eval()?),
+            Expr::Add(a, b) => Ok(a.eval()? + b.eval()?),
+        }
+    }
+}
+
+fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
+    let int = text::int(10)
+        .map(|s: String| Expr::Num(s.parse().unwrap()))
+        .padded();
+
+    int.then_ignore(end())
 }
