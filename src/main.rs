6f3eb9dd3d0dbae49e75214780852ae8eb275b6d
Isaac Woods <isaacwoods.home@gmail.com>
2023-01-15 02:13:47 +0000

    Handle parentheses by making the expression parser recursive

--- src/main.rs
+++ src/main.rs
@@ -46,40 +46,45 @@ impl Expr {
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let int = text::int(10)
-        .map(|s: String| Expr::Num(s.parse().unwrap()))
-        .padded();
+    recursive(|expr| {
 
-    let atom = int;
 
-    let op = |c| just(c).padded();
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
 
-    let unary = op('-')
-        .repeated()
-        .then(atom)
-        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+        let atom = int.or(expr.delimited_by(just('('), just(')'))).padded();
 
-    let product = unary
-        .clone()
-        .then(
-            op('*')
-                .to(Expr::Mul as fn(_, _) -> _)
-                .or(op('/').to(Expr::Div as fn(_, _) -> _))
-                .then(unary)
-                .repeated(),
-        )
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        let op = |c| just(c).padded();
 
-    let sum = product
-        .clone()
-        .then(
-            op('+')
-                .to(Expr::Add as fn(_, _) -> _)
-                .or(op('-').to(Expr::Sub as fn(_, _) -> _))
-                .then(product)
-                .repeated(),
-        )
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    sum.then_ignore(end())
+        let product = unary
+            .clone()
+            .then(
+                op('*')
+                    .to(Expr::Mul as fn(_, _) -> _)
+                    .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                    .then(unary)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        let sum = product
+            .clone()
+            .then(
+                op('+')
+                    .to(Expr::Add as fn(_, _) -> _)
+                    .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                    .then(product)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        sum
+    })
+    .then_ignore(end())
 }
